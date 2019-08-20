30a2fb41aeee51f6e2cadf84a5a04ee7a211ae91
Andres Heredia <andresheredia444@gmail.com>
2019-08-20 10:52:40 -0500

    despliegue de contrato en rinkeby-test

---
+++ compile.js
@@ -0,0 +1,11 @@
+const path=require('path');
+const fs=require('fs');
+const solc=require('solc');
+
+const inboxPath=path.resolve(__dirname, 'contracts', 'Inbox.sol');
+console.log(inboxPath);
+const source=fs.readFileSync(inboxPath, 'utf8');
+
+
+module.exports=solc.compile(source,1).contracts[':Inbox'];
+//console.log(module.exports);
